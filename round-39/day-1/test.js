876d95c8e8ef3bb7c574ab5e971e7e55720b7065
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-10-21 17:08:08 +0600

    js====

---
+++ round-39/day-1/test.js
@@ -0,0 +1,21 @@
+<!-- print way-->
+document.write("Hello World"+"<br/>");
+document.write("<br/>");
+<!-- print way-->
+var x = 10;
+var y = 15;
+var result = x + y;
+document.write("Folafol= "+result+"<br/>");
+<!-- print way-->
+var str = "Hello World"+"<br/>";
+document.write(str);
+<!-- print way-->
+
+var multi = x * y;
+console.log("==========Result====== "+multi);
+<!-- print way-->
+var decleration="10 flags";
+//Single line comments in Js
+/*
+multiple lines js comments
+*/
